0b2d22c75ccacde73f68dedf4138469e2e3b9092
Eduardo <vkurieduardo@gmail.com>
2021-04-04 17:11:56 -0500

    Dataframe con tweets de @ConacytYa

--- main.R
+++ main.R
@@ -1,7 +1,14 @@
-library(twitteR)
+require(twitteR)
+require(caret)
 
 apikey = "8DYohFObEbFw1BC36iUhulKJk"
 apisec = "EtnqNrsb4ZIFJ5WyfVu6STuteirjdELtL8H7XEJOadna2z7bJb"
 apitoken = "1307101446094102529-QEPqfbL7fSFuiGwdwlvNzetBxY7Hdz"
 tsec = "F6nygvrhoVwnkk6pzB9HBLS1xAXt5QLeMNmrQLIpHn7uR"
 setup_twitter_oauth(apikey, apisec, apitoken, tsec)
+
+conacyt <- userTimeline("ConacytYa", n = 150)
+
+
+
+df <- twListToDF(conacyt)
