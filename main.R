01eedc744797f7abdcc9d468025ab66b9ce1a152
Eduardo <vkurieduardo@gmail.com>
2021-04-05 21:31:06 -0500

    Creación de DF con los datos purificados de la TL de @ConacytYa

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 require(twitteR)
 require(caret)
+require(stringr)
 
 apikey = "8DYohFObEbFw1BC36iUhulKJk"
 apisec = "EtnqNrsb4ZIFJ5WyfVu6STuteirjdELtL8H7XEJOadna2z7bJb"
@@ -10,5 +11,10 @@ setup_twitter_oauth(apikey, apisec, apitoken, tsec)
 conacyt <- userTimeline("ConacytYa", n = 150)
 
 
-
 df <- twListToDF(conacyt)
+df$text <- str_replace_all(df$text, "[^[:alnum:]]", "")
+
+datos_conacyt <- data.frame(fecha=as.Date(trunc(df$created, "day")), texto = factor(tolower(df$text)))
+
+levels(datos_conacyt$texto) <- c(0,1)
+
