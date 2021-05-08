51e0c375f6d1108c40bcf7bdab2d17088c29dafb
Eduardo <vkurieduardo@gmail.com>
2021-05-08 18:31:39 -0500

    An exta script was added so that the API keys remain hidden

--- main.R
+++ main.R
@@ -2,14 +2,11 @@ require(twitteR)
 require(caret)
 require(stringr)
 require(dplyr)
+source('~/UNAM 2020-24/DATSCI/r/ConacytPred/ConacytPred/api.R')
 
-apikey = "8DYohFObEbFw1BC36iUhulKJk"
-apisec = "EtnqNrsb4ZIFJ5WyfVu6STuteirjdELtL8H7XEJOadna2z7bJb"
-apitoken = "1307101446094102529-QEPqfbL7fSFuiGwdwlvNzetBxY7Hdz"
-tsec = "F6nygvrhoVwnkk6pzB9HBLS1xAXt5QLeMNmrQLIpHn7uR"
-setup_twitter_oauth(apikey, apisec, apitoken, tsec)
 
-conacyt <- userTimeline("ConacytYa", n = 150)
+
+conacyt <- api()
 
 
 df <- twListToDF(conacyt)
