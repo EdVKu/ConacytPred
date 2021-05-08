e3a77ef44db4486a30fba9906092dab172750610
Eduardo <vkurieduardo@gmail.com>
2021-05-08 17:17:00 -0500

    Se puede percibir ahora la diferencia de días entre cada par de pagos

--- main.R
+++ main.R
@@ -1,6 +1,7 @@
 require(twitteR)
 require(caret)
 require(stringr)
+require(dplyr)
 
 apikey = "8DYohFObEbFw1BC36iUhulKJk"
 apisec = "EtnqNrsb4ZIFJ5WyfVu6STuteirjdELtL8H7XEJOadna2z7bJb"
@@ -16,9 +17,21 @@ df$text <- str_replace_all(df$text, "[^[:alnum:]]", "")
 
 diafe <- as.Date(trunc(df$created, "day"))
 
-datos_conacyt <- data.frame(ID = 1:length(df$text), fecha=diafe, dias_hoy = as.numeric(Sys.Date()-diafe),
+datos_conacyt <- data.frame(fecha=diafe, dias_hoy = as.numeric(Sys.Date()-diafe),
                             depo = factor(tolower(df$text)))
 
 
 levels(datos_conacyt$depo) <- c(0,1)
+
+pagosConacyt <-  datos_conacyt[datos_conacyt$depo==1,]
+
+dif <- c(datos_conacyt$dias_hoy[1])
+
+for (i in 1:length(pagosConacyt$depo)-1) {
+  dif <- append(dif,(pagosConacyt$dias_hoy[i+1]-pagosConacyt$dias_hoy[i]))
+}
+
+
+pagosConacyt <- pagosConacyt %>% mutate(difdias = dif)
+
 
