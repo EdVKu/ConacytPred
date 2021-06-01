defe8cbef84ebbd12b6a4251970c6341f456fc26
Eduardo <vkurieduardo@gmail.com>
2021-06-01 18:48:35 -0500

    Added confidence intervals. Last prediction had an error of 2 days.

--- main.R
+++ main.R
@@ -34,9 +34,15 @@ pagosConacyt <- pagosConacyt %>% mutate(difdias = dif)
 
 y = pagosConacyt$difdias[-1]
 x = seq(ldif,1,-1)
-
+md = mean(y)
+sdd = sd(y)
 lmod <- lm(y~x)
 
 proxpago <- as.numeric(lmod$coefficients[1] + (lmod$coefficients[2]*(ldif+2))-pagosConacyt$difdias[1])
 
-print(paste("The next payment will be on/El próximo pago será el", as.character(Sys.Date() + proxpago), sep = " "))
+intcon <- md + c(1,-1)*qt(0.975,ldif)*sdd/sqrt(ldif)
+
+print(paste(("The next payment will be on/El proximo pago sera el:"), 
+            as.character(Sys.Date() + proxpago),
+            ("the minimum and maximum dates, given a 95% confidence interval will be/las fechas minima y m�xima dado un intervalo del 95% de confianza son:"),
+            as.character(Sys.Date() + intcon[2]),"y" , as.character(Sys.Date() + intcon[1]), sep = " "))
