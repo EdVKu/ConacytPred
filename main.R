e0c61c43ebcc30ef0733429cff59ba2395449c17
Eduardo <vkurieduardo@gmail.com>
2021-06-06 16:02:44 -0500

    Preparing for the bot

--- main.R
+++ main.R
@@ -34,15 +34,22 @@ pagosConacyt <- pagosConacyt %>% mutate(difdias = dif)
 
 y = pagosConacyt$difdias[-1]
 x = seq(ldif,1,-1)
-md = mean(y)
+md = mean(y[-1])
 sdd = sd(y)
 lmod <- lm(y~x)
 
 proxpago <- as.numeric(lmod$coefficients[1] + (lmod$coefficients[2]*(ldif+2))-pagosConacyt$difdias[1])
 
-intcon <- md + c(1,-1)*qt(0.975,ldif)*sdd/sqrt(ldif)
+intcon <- proxpago + c(-1,1)*qt(0.975,ldif)*sdd/sqrt(ldif)
+
+fechapago = Sys.Date() + proxpago
+limspago = Sys.Date() + intcon
 
 print(paste(("The next payment will be on/El proximo pago sera el:"), 
             as.character(Sys.Date() + proxpago),
-            ("the minimum and maximum dates, given a 95% confidence interval will be/las fechas minima y máxima dado un intervalo del 95% de confianza son:"),
-            as.character(Sys.Date() + intcon[2]),"y" , as.character(Sys.Date() + intcon[1]), sep = " "))
+            ("the minimum and maximum dates, given a 95% confidence interval will be/las fechas minima y maxima dado un intervalo del 95% de confianza son:"),
+            as.character(limspago[1]),"y" , as.character(limspago[2]), sep = " "))
+
+return(c(fechapago, limspago))
+
+
