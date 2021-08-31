ee785cb3fba830a51c3f5993544ffa654dfe5a0a
Eduardo <vkurieduardo@gmail.com>
2021-08-31 12:35:50 -0500

    Remove outliers to try and improve precision

--- main.R
+++ main.R
@@ -32,8 +32,10 @@ for (i in 1:ldif) {
 
 pagosConacyt <- pagosConacyt %>% mutate(difdias = dif)
 
-y = pagosConacyt$difdias[-1]
-x = seq(ldif,1,-1)
+y0 = pagosConacyt$difdias[-1]
+y = y0[-9]
+x0 = seq(ldif,1,-1)
+x = x0[-9]
 md = mean(y[-1])
 sdd = sd(y)
 lmod <- lm(y~x)
@@ -45,11 +47,22 @@ intcon <- proxpago + c(-1,1)*qt(0.975,ldif)*sdd/sqrt(ldif)
 fechapago = Sys.Date() + proxpago
 limspago = Sys.Date() + intcon
 
-print(paste(("The next payment will be on/El proximo pago sera el:"), 
-            as.character(Sys.Date() + proxpago),
-            ("the minimum and maximum dates, given a 95% confidence interval will be/las fechas minima y maxima dado un intervalo del 95% de confianza son:"),
-            as.character(limspago[1]),"y" , as.character(limspago[2]), sep = " "))
+msjpago = paste(("The next payment will be (approximately) on/El proximo pago sera (aproximadamente) el:"), 
+                as.character(Sys.Date() + proxpago),
+                ("the minimum and maximum dates, given a 95% confidence interval will be/las fechas minima y maxima dado un intervalo del 95% de confianza son:"),
+                as.character(limspago[1]),"y" , as.character(limspago[2]), sep = " ")
 
-return(c(fechapago, limspago))
+
+
+predpagos <- function(pp, lp){
+  
+  return(paste(("The next payment will be on/El proximo pago sera el:"), 
+               as.character(Sys.Date() + pp),
+               ("the minimum and maximum dates, given a 95% confidence interval will be/las fechas minima y maxima dado un intervalo del 95% de confianza son:"),
+               as.character(limspago[1]),"y" , as.character(lp[2]), sep = " "))
+  
+}
+
+print(predpagos(proxpago, limspago))
 
 
